ab30a8d59d3ac6b75237811488cdf0573a11c506
omarsagoo <om.sagoo@gmail.com>
2021-04-12 15:22:48 -0700

    users can search for pets with a query

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
   /* GET home page. */
   app.get('/', (req, res) => {
     Pet.find().exec((err, pets) => {
-      res.render('pets-index', { pets: pets });    
+      res.render('pets-index', { pets: pets });
     });
   });
 }
--- routes/pets.js
+++ routes/pets.js
@@ -4,55 +4,67 @@ const Pet = require('../models/pet');
 // PET ROUTES
 module.exports = (app) => {
 
-  // INDEX PET => index.js
+    // INDEX PET => index.js
 
-  // NEW PET
-  app.get('/pets/new', (req, res) => {
-    res.render('pets-new');
-  });
-
-  // CREATE PET
-  app.post('/pets', (req, res) => {
-    var pet = new Pet(req.body);
-
-    pet.save()
-      .then((pet) => {
-        res.redirect(`/pets/${pet._id}`);
-      })
-      .catch((err) => {
-        // Handle Errors
-      }) ;
-  });
-
-  // SHOW PET
-  app.get('/pets/:id', (req, res) => {
-    Pet.findById(req.params.id).exec((err, pet) => {
-      res.render('pets-show', { pet: pet });
+    // NEW PET
+    app.get('/pets/new', (req, res) => {
+        res.render('pets-new');
     });
-  });
 
-  // EDIT PET
-  app.get('/pets/:id/edit', (req, res) => {
-    Pet.findById(req.params.id).exec((err, pet) => {
-      res.render('pets-edit', { pet: pet });
+    // CREATE PET
+    app.post('/pets', (req, res) => {
+        var pet = new Pet(req.body);
+
+        pet.save()
+            .then((pet) => {
+                res.redirect(`/pets/${pet._id}`);
+            })
+            .catch((err) => {
+                // Handle Errors
+            }) ;
     });
-  });
 
-  // UPDATE PET
-  app.put('/pets/:id', (req, res) => {
-    Pet.findByIdAndUpdate(req.params.id, req.body)
-      .then((pet) => {
-        res.redirect(`/pets/${pet._id}`)
-      })
-      .catch((err) => {
-        // Handle Errors
-      });
-  });
-
-  // DELETE PET
-  app.delete('/pets/:id', (req, res) => {
-    Pet.findByIdAndRemove(req.params.id).exec((err, pet) => {
-      return res.redirect('/')
+    // SHOW PET
+    app.get('/pets/:id', (req, res) => {
+        Pet.findById(req.params.id).exec((err, pet) => {
+            res.render('pets-show', { pet: pet });
+        });
     });
-  });
+
+    // EDIT PET
+    app.get('/pets/:id/edit', (req, res) => {
+        Pet.findById(req.params.id).exec((err, pet) => {
+            res.render('pets-edit', { pet: pet });
+        });
+    });
+
+    // UPDATE PET
+    app.put('/pets/:id', (req, res) => {
+        Pet.findByIdAndUpdate(req.params.id, req.body)
+            .then((pet) => {
+                res.redirect(`/pets/${pet._id}`)
+            })
+            .catch((err) => {
+                // Handle Errors
+            });
+    });
+
+    // DELETE PET
+    app.delete('/pets/:id', (req, res) => {
+        Pet.findByIdAndRemove(req.params.id).exec((err, pet) => {
+            return res.redirect('/')
+        });
+    });
+
+    // SEARCH PET
+    app.get('/search', (req, res) => {
+        term = new RegExp(req.query.term, 'i')
+    
+        Pet.find({$or:[
+            {'name': term},
+            {'species': term}
+        ]}).exec((err, pets) => {
+            res.render('pets-index', { pets: pets });
+        })
+    });
 }
